f01c68dd0e553a3d0d4d0d03146d5b7edb855ceb
in-yh <jyh1004jw@gmail.com>
2022-11-02 00:54:33 +0900

    221102

---
+++ Vue/01_Vue/Vue/13_vue_styleguide/00_vue_intro.vue
@@ -0,0 +1,22 @@
+<template>
+  <!-- HTML -->
+  <div>
+    <p>Hello :)</p>
+  </div>
+</template>
+
+<script>
+  // JavaScript
+</script>
+
+<style>
+  /* CSS */
+  p {
+    color: black;
+  }
+</style>
+
+// v-for는 항상 key와 함께 사용하기
+//  내부 컴포넌트의 상태를 일관되게 유지하기 위해 v-for에 항상 key를 사용하기
+//  데이터의 예측 가능한 행동을 유지 시키기(객체 불변성)
+// v-for를 쓴 element에 절대 v-if를 사용하지 말기
---
+++ Vue/01_Vue/Vue/13_vue_styleguide/02_2_v-for_hitdden.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <h1>2-2. 숨김 목록의 렌더링을 피할 때</h1>
+  
+  <!-- v-if를 컨테이너 엘리먼트로 옮기기 -->
+
+  <!-- bad -->
+  <!-- <ul>
+    <li
+      v-for="user in users"
+      v-if="shouldShowUsers"
+      :key="user.id"
+    >
+      {{ user.name }}
+    </li>
+  </ul> -->
+
+
+
+  <!-- good -->
+  <ul v-if="shouldShowUsers">
+    <li
+      v-for="user in users"
+      :key="user.id"
+    >
+      {{ user.name }}
+    </li>
+  </ul>
+
+
+</template>
+
+<script>
+  
+</script>
