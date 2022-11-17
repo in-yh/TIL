0edb7ac1c17394d1cc79bfe9ae785e03f1e3daaf
in-yh <jyh1004jw@gmail.com>
2022-11-17 21:51:45 +0900

    221117

--- Project/final-pjt/movies/urls.py
+++ Project/final-pjt/movies/urls.py
@@ -5,4 +5,6 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('category/genre/', views.category_genre, name='category_genre'),
+    path('category/genre/<str:genre_name>/', views.category_genre_detail, name='category_genre_detail'),
+    path('movie/<int:movie_pk>/', views.movie_detail, name='movie_detail'),
 ]
--- Project/final-pjt/movies/views.py
+++ Project/final-pjt/movies/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Count
-from django.http import JsonResponse
-from .models import Genre
+from .models import Genre, Movie
+import json, random
 
 # @require_safe
 def index(request): 
@@ -24,7 +24,46 @@ def category_genre(request):
                 'value': q.movies__count
             }
         )
+    j_results = json.dumps(results) # json으로 담기
     context = {
-        'results': results,
+        'j_results': j_results,
     }
     return render(request, 'movies/category_genre.html', context)
+    
+
+def category_genre_detail(request, genre_name):
+    genre = Genre.objects.get(name=genre_name)
+    movies = genre.movies.all()
+    random_movies = random.choices(movies, k=40)
+
+    movies_dict = []
+    for movie in random_movies:
+        movies_dict.append(
+            {
+                'x': movie.title, 
+                'y': movie.pk, # 영화 디테일 페이지로 들어가기 위해 필요
+                'value' : movie.vote_avg
+            }
+        )
+    j_results = json.dumps(movies_dict)
+    context = {
+        'j_results': j_results,
+    }
+    return render(request, 'movies/category_genre_detail.html', context)
+
+
+def movie_detail(request, movie_pk):
+    movie = Movie.objects.get(pk=movie_pk)
+
+    movie_information = [
+        {'x': movie.title, 'y': movie.poster_path, 'value' : 100},
+        {'x': movie.vote_avg, 'y': movie.poster_path, 'value' : 100},
+        {'x': movie.popularity, 'y': movie.poster_path, 'value' : 100},
+        {'x': str(movie.released_date)[0:10], 'y': movie.poster_path, 'value' : 100},
+        # {'x': str(movie.overview), 'value' : 100},
+    ]
+    j_results = json.dumps(movie_information)
+    context = {
+        'j_results': j_results,
+    }
+    return render(request, 'movies/movie_detail.html', context)
